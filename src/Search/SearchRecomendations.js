3ddd06db60bb875b096a5fce2555a4b16c68f515
chloe <chloeleichen@gmail.com>
2016-11-28 15:52:39 +1100

    fix file path

---
+++ src/Search/SearchRecomendations.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+import './SearchRecomendations.css';
+
+class SearchRecomendations extends Component {
+  render() {
+    return (
+      <div className='search-recomendation'></div>
+    );
+  }
+}
+SearchRecomendations.propTypes = {};
+
+
+export default SearchRecomendations;
