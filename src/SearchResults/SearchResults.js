0f9ddca3a5ade38ab2146a2620ddd16718918705
chloe <chloeleichen@gmail.com>
2016-12-06 15:54:38 +1100

    more mobile

--- src/SearchResults/SearchResults.js
+++ src/SearchResults/SearchResults.js
@@ -31,7 +31,7 @@ class SearchResults extends Component {
   getSummaryText(){
     if(this.props.searchResults.length){
       return (
-          <div className='search-results-count hidden-xs'>
+          <div className='search-results-count'>
             <h4><strong>{this.props.totalNumberOfResults} results found</strong></h4>
           </div>);
     }
