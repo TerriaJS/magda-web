a96adf8496d3526270066d5e66738a924e6ffc03
chloe <chloeleichen@gmail.com>
2017-06-05 16:21:00 +1000

    twitter function

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -1,14 +1,18 @@
+//@flow
 import React from 'react';
 import './Social.css';
 
 
 export default class Social extends React.Component {
   render() {
+    const url: string = window.location.href;
     return (
       <div className="social">
-        <div><button className='btn btn-default'><i className="fa fa-star" aria-hidden="true"></i>Star<span className="count">0</span></button></div>
         <div><button className='btn btn-default'><i className="fa fa-rss" aria-hidden="true"></i>Subscribe<span className="count">0</span></button></div>
-        <div><button className='btn btn-default'><i className="fa fa-share-alt" aria-hidden="true"></i>Share<span className="count">0</span></button></div>
+        <div>
+          <a className="twitter-share-button btn btn-default" href={`https://twitter.com/intent/tweet?url=${url}`}
+          data-size="large"><i className="fa fa-twitter" aria-hidden="true"></i>Tweet</a>
+        </div>
       </div>
     );
   }
